1ecc2b373a50e3a4b9cccb41efe7c2c157882b48
Václav Cvrček <vaclav.cvrcek@ff.cuni.cz>
2020-12-07 17:37:10 +0100

    Version 0.2

--- global.R
+++ global.R
@@ -8,7 +8,7 @@ library(stringr)
 
 httr::set_config(httr::config(http_version = 0)) # problem s nginex
 appName <- "QuitaUp"
-appVer = c("11/2020" = "0.1")
+appVer = c("12/2020" = "0.2")
 #logFile = "access.log"
 bugReportUrl <- "https://podpora.korpus.cz/projects/quitaup"
 #enableBookmarking(store = "url")
